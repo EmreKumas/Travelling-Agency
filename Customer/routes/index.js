77f0fa581f6deb675b5c89715f9d5241adcc83ce
Emre Kumaş <emre-kumas@outlook.com>
2019-12-17 17:55:46 +0300

    Customer back-end: reserved, alternate and no_reservation responses

--- Customer/routes/index.js
+++ Customer/routes/index.js
@@ -4,7 +4,21 @@ const router = express.Router();
 
 // All GET and POST requests are handled here...
 router.post('/', (req, res) => {
-    res.send(req.body);
+    
+    var reservation = 'alternate';
+    var response = req.body;
+
+    if(reservation === 'reserved'){
+        response['reserved'] = 'reserved';
+    }else if(reservation === 'alternate'){
+        response['reserved'] = 'alternate';
+        response['hotel'] = '10';
+        response['airline'] = '20';
+    }else if(reservation === 'no_reservation'){
+        response['reserved'] = 'no_reservation';
+    }
+    
+    res.send(response);
 });
 
 module.exports = router;
