9bed315a7414acf179bf811cfa7f0c431647cdbd
Emre Kumaş <emre-kumas@outlook.com>
2019-12-20 08:20:00 +0300

    Agency-customer TCP connection fully established

--- Agency/main.py
+++ Agency/main.py
@@ -1,9 +1,22 @@
 import socket
+import json
 
 
 # ########################################### CONSTANTS
 HOSTNAME = "localhost"
 PORT = 80
+
+
+# ########################################### FUNCTIONS
+# noinspection PyShadowingNames
+def process_customer_data(data):
+
+    customer_data = json.loads(data)
+
+    # Add a reserved field to indicate if it is reserved or not.
+    customer_data['reserved'] = 'reserved'
+
+    return json.dumps(customer_data, ensure_ascii=False), customer_data['name']
 
 
 if __name__ == "__main__":
@@ -12,25 +25,26 @@ if __name__ == "__main__":
     server.bind((HOSTNAME, PORT))
     server.listen(5)
 
+    # Empty string for customer name...
+    customer_name = ""
+
     # While server is running...
     while True:
         connection, address = server.accept()
-        customer_data = ""
 
         # While a connection is established...
         while True:
-            data = connection.recv(4096).decode("utf8")
+            customer_data = connection.recv(4096).decode("utf8")
             # If there is no data, exit the loop...
-            if not data:
+            if not customer_data:
                 break
 
-            # Concatenate the data...
-            customer_data += data
-            print(customer_data)
+            # Process customer data...
+            customer_data, customer_name = process_customer_data(customer_data)
 
             # And, send a response back to the customer...
-            connection.send(bytes('Some text', encoding="utf8"))
+            connection.send(bytes(customer_data, encoding="utf8"))
 
         # Close the connection...
         connection.close()
-        print("Customer disconnected!")
+        print("Customer with the name '" + customer_name + "' is disconnected!")
