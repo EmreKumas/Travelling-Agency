8a59597deaed81612d5765e3528d1b89da4a8283
Emre Kumaş <emre-kumas@outlook.com>
2019-12-22 16:33:24 +0300

    Customer gets all hotel names by connecting to the agency

--- Customer/routes/index.js
+++ Customer/routes/index.js
@@ -6,7 +6,20 @@ const router = express.Router();
 // All GET and POST requests are handled here...
 router.get('/', (req, res) => {
 
-    res.render('index');
+    // Get all hotel names by connecting to the agency...
+    var request = {'hotel_names': true};
+
+    var customer = new Customer();
+    
+    if(customer.connected){
+        var server_response = customer.send_data(request);
+
+        // After server response arrives...
+        server_response.then(response => {
+            res.render('index', response);
+        });
+    }else
+        res.render(null);
 });
 
 router.post('/', (req, res) => {
